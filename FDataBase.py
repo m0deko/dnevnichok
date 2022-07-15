a3e2cf5c9afc13a10e5b4b4ccf85f7bb20a6e58b
Sergei Sterlygov <sterlygovs@gmail.com>
2022-07-15 19:01:40 +0300

    Изменения в таблицы

--- FDataBase.py
+++ FDataBase.py
@@ -4,77 +4,77 @@ class FDataBase():
     def __init__(self, db):
         self.__db = db
         self.__cur = db.cursor()
-    def getMenu(self, username):
-        sql = f'''SELECT * FROM maintable WHERE username = "{username}"'''
-        try:
-            self.__cur.execute(sql)
-            res = self.__cur.fetchall()
+    # def getMenu(self, username):
+    #     sql = f'''SELECT * FROM users_data WHERE username = "{username}"'''
+    #     try:
+    #         self.__cur.execute(sql)
+    #         res = self.__cur.fetchall()
+    #
+    #         return [item for item in res[0]]
+    #     except Exception as ex:
+    #         print('Ошибка чтения из БД', ex)
+    #     return []
+    # def getTeacher(self, username):
+    #     sql = f'''SELECT * FROM teacher_maintable WHERE username = "{username}"'''
+    #     try:
+    #         self.__cur.execute(sql)
+    #         res = self.__cur.fetchall()
+    #         return [item for item in res[0]]
+    #     except Exception as ex:
+    #         print('Ошибка чтения из БД', ex)
+    #     return []
 
-            return [item for item in res[0]]
-        except Exception as ex:
-            print('Ошибка чтения из БД', ex)
-        return []
-    def getTeacher(self, username):
-        sql = f'''SELECT * FROM teacher_maintable WHERE username = "{username}"'''
-        try:
-            self.__cur.execute(sql)
-            res = self.__cur.fetchall()
-            return [item for item in res[0]]
-        except Exception as ex:
-            print('Ошибка чтения из БД', ex)
-        return []
-
-    def addPost(self, username, psw, email, surname, name, second_name, city, school_num, school_class):
-        try:
-            self.__cur.execute(
-                '''INSERT INTO maintable(username, password, email, surname, realname, second_name, city, school_num, school_class)
-                 VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-                (username, psw, email, surname, name, second_name, city, school_num, school_class))
-            self.__db.commit()
-        except sqlite3.Error as ex:
-            print(str(ex))
-            return False
-        return True
-
-    def getLes(self, num):
-        try:
-            self.__cur.execute(f'''SELECT * FROM lessons WHERE class = {num}''')
-            res = self.__cur.fetchall()
-            return [item for item in res[0]]
-        except Exception as ex:
-            print(ex)
-            return ''
-    def addTeacher(self, username, psw, email, surname, name, second_name, city, school_num, school_class, subject):
-        try:
-            self.__cur.execute(
-                '''INSERT INTO teacher_maintable(username, password, email, surname, realname, second_name, city, school_num, school_class, subject)
-                 VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-                (username, psw, email, surname, name, second_name, city, school_num, school_class, subject))
-            self.__db.commit()
-        except sqlite3.Error as ex:
-            print(str(ex))
-            return False
-        return True
-    def getMenuForTeacher(self, username):
-        sql = f'''SELECT * FROM teacher_maintable WHERE username = "{username}"'''
-        try:
-            self.__cur.execute(sql)
-            res = self.__cur.fetchall()
-            return [item for item in res[0]]
-        except Exception as ex:
-            print('Ошибка чтения из БД', ex)
-        return []
-    def getClass(self, class_num):
-        try:
-            self.__cur.execute(f'''SELECT surname FROM maintable WHERE school_class = "{class_num}"''')
-            res = self.__cur.fetchall()
-            result = []
-            for x in range(len(res)):
-                result += res[x]
-            return result
-        except Exception as ex:
-            print('Ошибка чтения бд', ex)
-        return []
+    # def addPost(self, username, psw, email, surname, name, second_name, city, school_num, school_class):
+    #     try:
+    #         self.__cur.execute(
+    #             '''INSERT INTO users_data(group_id,username, password, email, surname, name, second_name, city, law)
+    #              VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+    #             (username, psw, email, surname, name, second_name, city, school_num, school_class))
+    #         self.__db.commit()
+    #     except sqlite3.Error as ex:
+    #         print(str(ex))
+    #         return False
+    #     return True
+    #
+    # def getLes(self, num):
+    #     try:
+    #         self.__cur.execute(f'''SELECT * FROM lessons WHERE class = {num}''')
+    #         res = self.__cur.fetchall()
+    #         return [item for item in res[0]]
+    #     except Exception as ex:
+    #         print(ex)
+    #         return ''
+    # def addTeacher(self, username, psw, email, surname, name, second_name, city, school_num, school_class, subject):
+    #     try:
+    #         self.__cur.execute(
+    #             '''INSERT INTO teacher_maintable(username, password, email, surname, realname, second_name, city, school_num, school_class, subject)
+    #              VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+    #             (username, psw, email, surname, name, second_name, city, school_num, school_class, subject))
+    #         self.__db.commit()
+    #     except sqlite3.Error as ex:
+    #         print(str(ex))
+    #         return False
+    #     return True
+    # def getMenuForTeacher(self, username):
+    #     sql = f'''SELECT * FROM teacher_maintable WHERE username = "{username}"'''
+    #     try:
+    #         self.__cur.execute(sql)
+    #         res = self.__cur.fetchall()
+    #         return [item for item in res[0]]
+    #     except Exception as ex:
+    #         print('Ошибка чтения из БД', ex)
+    #     return []
+    # def getClass(self, class_num):
+    #     try:
+    #         self.__cur.execute(f'''SELECT surname FROM maintable WHERE school_class = "{class_num}"''')
+    #         res = self.__cur.fetchall()
+    #         result = []
+    #         for x in range(len(res)):
+    #             result += res[x]
+    #         return result
+    #     except Exception as ex:
+    #         print('Ошибка чтения бд', ex)
+    #     return []
     # def getMarks(self, class_num, subject):
     #     try:
     #         self.__cur.execute(f'''SELECT mark FROM marks WHERE id = {class_num} AND lesson = "{subject}"''')
@@ -86,26 +86,50 @@ class FDataBase():
     #     except Exception as ex:
     #         print('Ошибка чтения бд', ex)
     #     return []
-    def getID(self, username):
+    # def getID(self, username):
+    #     try:
+    #         self.__cur.execute(f'''SELECT id FROM maintable WHERE username = "{username}"''')
+    #         res = self.__cur.fetchall()
+    #         result = []
+    #         for x in range(len(res)):
+    #             result += res[x]
+    #             print(result)
+    #         return res[0]
+    #     except Exception as ex:
+    #         print('Ошибка чтения бд', ex)
+    #     return []
+    #
+    # def getMark(self, les, cur_id):
+    #     try:
+    #         self.__cur.execute(f'''SELECT mark FROM marks WHERE id = {cur_id} AND lesson = "{les}"''')
+    #         res = self.__cur.fetchall()
+    #         result = []
+    #         for x in range(len(res)):
+    #             result += res[x]
+    #         return result
+    #     except Exception as ex:
+    #         return []
+    def getGroupID(self, school, grade):
         try:
-            self.__cur.execute(f'''SELECT id FROM maintable WHERE username = "{username}"''')
-            res = self.__cur.fetchall()
-            result = []
-            for x in range(len(res)):
-                result += res[x]
-                print(result)
-            return res[0]
+            self.__cur.execute(f'''SELECT ROWID FROM schoolGrade WHERE school = "{school}" AND grade = "{grade}"''')
+            preres = self.__cur.fetchone()
+            if preres != None:
+                return [x for x in preres][0]
+            else:
+                self.__cur.execute('''INSERT into schoolGrade(school, grade) VALUES (?, ?)''', (school, grade))
+                self.__db.commit()
+                self.__cur.execute(f'''SELECT ROWID FROM schoolGrade WHERE school = "{school}" AND grade = "{grade}"''')
+                preres = self.__cur.fetchone()
+                return [x for x in preres][0]
         except Exception as ex:
-            print('Ошибка чтения бд', ex)
-        return []
-
-    def getMark(self, les, cur_id):
+            print(ex)
+    def addStudent(self, username, password, email, surname, name, second_name, city, school, grade):
         try:
-            self.__cur.execute(f'''SELECT mark FROM marks WHERE id = {cur_id} AND lesson = "{les}"''')
-            res = self.__cur.fetchall()
-            result = []
-            for x in range(len(res)):
-                result += res[x]
-            return result
+            group_id = self.getGroupID(school, grade)
+            self.__cur.execute('''INSERT into users_data(group_id, username, password, email, surname, name, second_name, city, law) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)''', (group_id, username, password, email, surname, name, second_name, city, 0))
+            self.__db.commit()
+            print('successful')
+            # self.__cur.execute(f'''INSERT into users_data()
+            # ''')
         except Exception as ex:
-            return []
+            print(ex)
--- links.py
+++ links.py
@@ -1,162 +1,150 @@
 from configurations import *
 
-@app.route('/', methods=['GET', 'POST'])
-def start_page():
-    if session.get('logged_in') and ('ddata' in session):
-        return render_template('start_page.html', name=session["ddata"][5],
-                               day_ras=uroki[date_string], time=day_times,
-                               length=len(uroki[date_string]), day=date_string, surname = session['ddata'][4])
-    else:
-        session['logged_in'] = False
-        return redirect(url_for("login"))
 
-
+# @app.route('/', methods=['GET', 'POST'])
+# def start_page():
+#     if session.get('logged_in') and ('ddata' in session):
+#         return render_template('start_page.html', name=session["ddata"][5],
+#                                day_ras=uroki[date_string], time=day_times,
+#                                length=len(uroki[date_string]), day=date_string, surname=session['ddata'][4])
+#     else:
+#         session['logged_in'] = False
+#         return redirect(url_for("login"))
+# 
+# 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if session['logged_in']:
-        return redirect(url_for('start_page'))
-    elif request.method == 'POST':
-        db = get_db()
-        dbase = FDataBase(db)
-        session['ddata'] = dbase.getMenu(request.form['uname'])
-
-        if (session['ddata'] != []):
-            if request.form['psw'] == (session['ddata'][2]):
-                session['logged_in'] = True
-                session['id'] = session['ddata'][0]
-                session['username'] = request.form['uname']
-                session['lesson'] = dbase.getLes(int(session['ddata'][9][0]))
-
-                return redirect(url_for('start_page'))
+    # if session['logged_in']:
+    #     return redirect(url_for('start_page'))
+    # elif request.method == 'POST':
+    #     db = get_db()
+    #     dbase = FDataBase(db)
+    #     # session['ddata'] = dbase.getMenu(request.form['uname'])
+    #
+    #     if (session['ddata'] != []):
+    #         if request.form['psw'] == (session['ddata'][2]):
+    #             session['logged_in'] = True
+    #             session['id'] = session['ddata'][0]
+    #             session['username'] = request.form['uname']
+    #             session['lesson'] = dbase.getLes(int(session['ddata'][9][0]))
+    #
+    #             return redirect(url_for('start_page'))
 
     return render_template('login.html')
 
-@app.route('/loginTeacher', methods=['GET', 'POST'])
-def login_for_teacher():
-    if session['logged_in']:
-        return redirect(url_for('mark_input'))
-    elif request.method == 'POST':
-        db = get_db()
-        dbase = FDataBase(db)
-        session['ddata'] = dbase.getMenuForTeacher(request.form['uname'])
-        if (session['ddata'] != []):
-            if request.form['psw'] == session['ddata'][2]:
-                session['logged_in'] = True
-                session['id'] = session['ddata'][0]
-                session['username'] = request.form['uname']
-                session['class'] = dbase.getTeacher(session['ddata'][1])[9]
-                session['subject'] = dbase.getTeacher(session['ddata'][1])[10]
-                print(session['class'], session['subject'])
-                return redirect(url_for('mark_input'))
-    return render_template('login_teacher.html')
 
-@app.route('/register', methods=['POST', 'GET'])
+#
+# @app.route('/loginTeacher', methods=['GET', 'POST'])
+# def login_for_teacher():
+#     if session['logged_in']:
+#         return redirect(url_for('mark_input'))
+#     elif request.method == 'POST':
+#         db = get_db()
+#         dbase = FDataBase(db)
+#         session['ddata'] = dbase.getMenuForTeacher(request.form['uname'])
+#         if (session['ddata'] != []):
+#             if request.form['psw'] == session['ddata'][2]:
+#                 session['logged_in'] = True
+#                 session['id'] = session['ddata'][0]
+#                 session['username'] = request.form['uname']
+#                 session['class'] = dbase.getTeacher(session['ddata'][1])[9]
+#                 session['subject'] = dbase.getTeacher(session['ddata'][1])[10]
+#                 print(session['class'], session['subject'])
+#                 return redirect(url_for('mark_input'))
+#     return render_template('login_teacher.html')
+
+@app.route('/register/', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
         db = get_db()
         dbase = FDataBase(db)
-        if request.form['psw'] != request.form['psw-repeat']:
-            flash('', category='error')
-        elif not ('@' in request.form['email']):
-            flash('', category='error')
-        elif dbase.getMenu(request.form['username']) != [] or dbase.getMenu(request.form['email']) != []:
-            flash('Аккаунт с данной почтой или логином уже существует', category='error')
-        elif request.form['name'].isdigit() or request.form['second_name'].isdigit() or request.form['surname'].isdigit():
-            flash('', category='error')
-        else:
-            flash('Регистрация прошла успешно!', category='success')
-            surname = ''
-            name = ''
-            second_name = ''
-            city = ''
-            school_num = ''
-            school_class = ''
-            surname += request.form['surname']
-            name += request.form['name']
-            second_name += request.form['second_name']
-            city += request.form['city']
-            school_num += request.form['school_num']
-            school_class += request.form['school_class']
-            res = dbase.addPost(request.form['username'], request.form['psw'], request.form['email'],
-                                surname, name, second_name,
-                                city, school_num, school_class)
-    return render_template('register.html')
+        # dbase.addStudent("ste", '123', '@e', 'Стерлягов', 'Сергей', 'Дмитриевич', 'Москва', '1580', '8А')
+        # school_class += request.form['school_class']
+        # res = dbase.addPost(request.form['username'], request.form['psw'], request.form['email'],
+        #                     surname, name, second_name,
+        #                     city, school_num, school_class)
+        print(request.form.get('username_form'))
+    return render_template('new_register.html')
 
-@app.route('/register_teacher', methods=['POST', 'GET'])
-def register_for_teacher():
-    if request.method == 'POST':
-        db = get_db()
-        dbase = FDataBase(db)
-        if request.form['psw'] != request.form['psw-repeat']:
-            flash('', category='error')
-        elif not ('@' in request.form['email']):
-            flash('', category='error')
-        elif dbase.getMenuForTeacher(request.form['username']) != []:
-            flash('Аккаунт с данной почтой или логином уже существует', category='error')
-        else:
-            flash('Регистрация прошла успешно!', category='success')
-            surname = request.form['surname']
-            name = request.form['name']
-            second_name = request.form['second_name']
-            city = request.form['city']
-            school_num = request.form['school_num']
-            school_class = request.form['school_class']
-            subject = request.form['subject']
-            if subject == 'Математика' and int(school_class[0]) >= 7:
-                subject = 'Алгебра'
-            res = dbase.addTeacher(request.form['username'], request.form['psw'], request.form['email'],
-                                surname, name, second_name,
-                                city, school_num, school_class, subject)
-    return render_template('reg_teacher.html')
 
-@app.route('/marks', methods=['GET', 'POST'])
-def marks():
-    db = get_db()
-    dbase = FDataBase(db)
-    student_mark = {}
-    for les in session['lesson'][1].split():
-        mark_res = dbase.getMark(les, session['id'])
-        student_mark[les] = mark_res
-    sred_marks = middle_marks(student_mark)
-    if session.get('logged_in'):
-        return render_template('page_with_mark.html', name=session["ddata"][5],
-                               lessons=session['lesson'][1].split(),
-                               marks=student_mark, sred=sred_marks, surname = session['ddata'][4])
-    else:
-        return redirect(url_for("login"))
-
-# @app.route('/mark_input', methods=['GET', 'POST'])
-# def mark_input():
-#     if session.get('logged_in') and 'ddata' in session and 'class' in session:
+# @app.route('/register_teacher', methods=['POST', 'GET'])
+# def register_for_teacher():
+#     if request.method == 'POST':
 #         db = get_db()
 #         dbase = FDataBase(db)
-#         result = dbase.getClass(session['class'])
-#         mark = {}
-#         for child in result:
-#             print(child, dbase.getID(child[0]))
-#             child_id = dbase.getID(child[0])
-#             mark[child] = dbase.getMarks(child_id, session['class'])
-#         return render_template('mark_input.html', children_list = result, marks = mark, glav = session['subject'])
+#         if request.form['psw'] != request.form['psw-repeat']:
+#             flash('', category='error')
+#         elif not ('@' in request.form['email']):
+#             flash('', category='error')
+#         elif dbase.getMenuForTeacher(request.form['username']) != []:
+#             flash('Аккаунт с данной почтой или логином уже существует', category='error')
+#         else:
+#             flash('Регистрация прошла успешно!', category='success')
+#             surname = request.form['surname']
+#             name = request.form['name']
+#             second_name = request.form['second_name']
+#             city = request.form['city']
+#             school_num = request.form['school_num']
+#             school_class = request.form['school_class']
+#             subject = request.form['subject']
+#             if subject == 'Математика' and int(school_class[0]) >= 7:
+#                 subject = 'Алгебра'
+#             res = dbase.addTeacher(request.form['username'], request.form['psw'], request.form['email'],
+#                                    surname, name, second_name,
+#                                    city, school_num, school_class, subject)
+#     return render_template('reg_teacher.html')
+#
+#
+# @app.route('/marks', methods=['GET', 'POST'])
+# def marks():
+#     db = get_db()
+#     dbase = FDataBase(db)
+#     student_mark = {}
+#     for les in session['lesson'][1].split():
+#         mark_res = dbase.getMark(les, session['id'])
+#         student_mark[les] = mark_res
+#     sred_marks = middle_marks(student_mark)
+#     if session.get('logged_in'):
+#         return render_template('page_with_mark.html', name=session["ddata"][5],
+#                                lessons=session['lesson'][1].split(),
+#                                marks=student_mark, sred=sred_marks, surname=session['ddata'][4])
 #     else:
 #         return redirect(url_for("login"))
-
-@app.route('/lessons', methods=['GET', 'POST'])
-def lessons():
-    if session.get('logged_in'):
-        return render_template('page_with_lessons.html', name=session["ddata"][5], times=times1,
-                               raspis=uroki,
-                               length=len(times1), surname = session['ddata'][4])
-    else:
-        return redirect(url_for('login'))
-
-@app.route('/logout')
-def logout():
-    session['logged_in'] = False
-    if 'class' in session and 'subject' in session:
-        session.pop('class')
-        session.pop('subject')
-    elif 'lesson' in session:
-        session.pop('lesson')
-    session.pop('username')
-    session.pop('id')
-    return start_page()
+#
+#
+# # @app.route('/mark_input', methods=['GET', 'POST'])
+# # def mark_input():
+# #     if session.get('logged_in') and 'ddata' in session and 'class' in session:
+# #         db = get_db()
+# #         dbase = FDataBase(db)
+# #         result = dbase.getClass(session['class'])
+# #         mark = {}
+# #         for child in result:
+# #             print(child, dbase.getID(child[0]))
+# #             child_id = dbase.getID(child[0])
+# #             mark[child] = dbase.getMarks(child_id, session['class'])
+# #         return render_template('mark_input.html', children_list = result, marks = mark, glav = session['subject'])
+# #     else:
+# #         return redirect(url_for("login"))
+#
+# @app.route('/lessons', methods=['GET', 'POST'])
+# def lessons():
+#     if session.get('logged_in'):
+#         return render_template('page_with_lessons.html', name=session["ddata"][5], times=times1,
+#                                raspis=uroki,
+#                                length=len(times1), surname=session['ddata'][4])
+#     else:
+#         return redirect(url_for('login'))
+#
+#
+# @app.route('/logout')
+# def logout():
+#     session['logged_in'] = False
+#     if 'class' in session and 'subject' in session:
+#         session.pop('class')
+#         session.pop('subject')
+#     elif 'lesson' in session:
+#         session.pop('lesson')
+#     session.pop('username')
+#     session.pop('id')
+#     return start_page()
