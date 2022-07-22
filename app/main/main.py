39ad4b2e2d0df821b609a6262c042c46c7f8d57d
Sergei Sterlygov <sterlygovs@gmail.com>
2022-07-22 11:26:12 +0300

    Добавлен API регистрации и логина

--- app/main/main.py
+++ app/main/main.py
@@ -1,4 +1,4 @@
-from flask import request, session, redirect, url_for, render_template, Blueprint
+from flask import request, session, redirect, url_for, render_template, Blueprint, make_response
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from ..database import db
@@ -9,40 +9,33 @@ from ..models.mark import Mark
 from ..models.timetable import Timetable
 from ..models.homework import Homework
 
+from .action import middle_marks, png_check
+
 main = Blueprint('main', __name__, template_folder='templates', static_folder='static')
 
 
-# @main.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if ('logged' not in session):
-#         if request.method == 'POST':
-#             cur_id = base.getAccess(request.form['identification'], request.form['password'])
-#             cur_id = User_data.query
-#             print(cur_id)
-#             if cur_id != None:
-#
-#                 if 'sessionCheckbox' in request.form:
-#                     session.permanent = True
-#                 else:
-#                     session.permanent = False
-#                 session['logged'] = 1
-#                 session['id'] = cur_id
-#                 session['group_id'] = base.getGroupID(session['id'])
-#                 print(session['id'], session['group_id'])
-#                 return redirect(url_for('.mainpage'))
-#
-#             if session['user_id']:
-#                 if 'sessionCheckbox' in request.form:
-#                     session.permanent = True
-#                 else:
-#                     session.permanent = False
-#                 session['logged'] = True
-#                 # session['data'] = base.getData(session['user_id'])
-#                 session['group_id'] = str(session['data'][6])
-#                 return redirect(url_for('.mainpage'))
-#     elif session['logged']:
-#         return redirect(url_for('.mainpage'))
-#     return render_template('login.html')
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    if ('logged' not in session):
+        if request.method == 'POST':
+            # cur_id = base.getAccess(request.form['identification'], request.form['password'])
+            data = User_data.query.filter(
+                (User_data.username == request.form['identification']) and (
+                            User_data.email == request.form['identification'])).first()
+            if data:
+                if check_password_hash(data.psw, request.form['password']):
+                    if 'sessionCheckbox' in request.form:
+                        session.permanent = True
+                    else:
+                        session.permanent = False
+                    session['logged'] = 1
+                    session['id'] = data.id
+                    # session['group_id'] = base.getGroupID(session['id'])
+
+                    return redirect(url_for('.mainpage'))
+    elif session['logged']:
+        return redirect(url_for('.mainpage'))
+    return render_template('main/login.html')
 
 
 @main.route('/register', methods=['POST', 'GET'])
@@ -60,19 +53,18 @@ def register():
             print(ex)
     return render_template('main/register.html')
 
-#
-# @main.route('/', methods=['GET', 'POST'])
-# def mainpage():
-#     if ('logged' not in session):
-#         return redirect(url_for('login'))
-#     session['cur_page'] = 'mainpage'
-    # if request.method == "GET":
-    #     print(1)
+@main.route('/', methods=['GET', 'POST'])
+def mainpage():
+    if ('logged' not in session):
+        return redirect(url_for('.login'))
+    session['cur_page'] = 'mainpage'
+    if request.method == "GET":
+        print(1)
     # with open('dnevnik.json', encoding='utf-8') as f:
     #     timetable = json.load(f)['class_id'][session['group_id']]['timetable'][week_string]
-    # return render_template('new_mainpage.html')
-    # return render_template('new_mainpage.html', date_info=date_mas, cur_day_time=timetable, data=session['data'])
-#
+    return render_template('main/mainpage.html')
+    # return render_template('mainpage.html', date_info=date_mas, cur_day_time=timetable, data=session['data'])
+
 #
 # @app.route('/marks', methods=['GET', 'POST'])
 # def marks():
@@ -86,7 +78,7 @@ def register():
 #     #     mark_res = dbase.getMarks(1, les)
 #     #     student_mark.append([les, mark_res])
 #     # sred_marks = middle_marks(student_mark)
-#     # return render_template('new_markpage.html', all_les=sred_marks, all_marks=student_mark, data=session['data'])
+#     # return render_template('markpage.html', all_les=sred_marks, all_marks=student_mark, data=session['data'])
 #
 #
 # @app.route('/lessons', methods=['GET', 'POST'])
@@ -110,36 +102,35 @@ def register():
 #     return render_template("homework.html", data=session['data'], homework=homework)
 #
 #
-# @app.route('/userava')
-# def userava():
-#     if ('logged' not in session):
-#         return redirect(url_for('login'))
-#     # img = dbase.getAvatar(session['user_id'], app)
-#     # if not img:
-#     #     return ""
-#     # h = make_response(img)
-#     # h.headers['Content-Type'] = 'image/png'
-#     # return h
+@main.route('/userava')
+def userava():
+    if ('logged' not in session):
+        return redirect(url_for('login'))
+    img = User_data.query.filter(User_data.id == session['id']).first().avatar
+    if not img:
+        return ""
+    h = make_response(img)
+    h.headers['Content-Type'] = 'image/png'
+    return h
+
+
+@main.route('/upload', methods=['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and png_check(file.filename):
+            try:
+                img = file.read()
+                User_data.querry.filter(User_data==session['id']).update({User_data.avatar:img})
+            except Exception as ex:
+                print(ex)
+    # return redirect(url_for(session['cur_page']))
+    return "Hi"
 #
-#
-# @app.route('/upload', methods=['POST', 'GET'])
-# def upload():
-#     if request.method == 'POST':
-#         file = request.files['file']
-#     #     if file and png_check(file.filename):
-#     #         try:
-#     #             img = file.read()
-#     #             dbase.updateAvatar(img, session['user_id'])
-#     #         except Exception as ex:
-#     #             print(ex)
-#     # return redirect(url_for(session['cur_page']))
-#
-#
-# @app.route('/logout')
-# def logout():
-#     if 'logged' not in session:
-#         return redirect(url_for('login'))
-#     session.pop('logged')
-#     session.pop('id')
-#     session.pop('group_id')
-#     return redirect(url_for('login'))
+@main.route('/logout')
+def logout():
+    if 'logged' not in session:
+        return redirect(url_for('.login'))
+    session.pop('logged')
+    session.pop('id')
+    return redirect(url_for('.login'))
